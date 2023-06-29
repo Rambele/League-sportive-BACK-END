b571adedc6190e33d8b99153d450fbe0c940f288
Ait Slimane <rachid.a81@yahoo.com>
2023-06-29 16:00:43 +0200

    Creation du shema utilisateur et les crud

--- serveur.js
+++ serveur.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 const app = express();
 const port = 3000;
 
 // Middleware pour permettre à Express de traiter les données JSON
 app.use(express.json());
+
+
+
+// Schéma de l'utilisateur
+const userSchema = new mongoose.Schema({
+    nom: String,
+    prenom: String,
+    tel: String,
+    email: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    motDePasse: {
+      type: String,
+      required: true
+    },
+    panier: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Produit'
+    }]
+  });
+
+// Modèle de l'utilisateur basé sur le schéma
+const User = mongoose.model('User', userSchema);
 
 // Schéma du produit
 const produitSchema = new mongoose.Schema({
@@ -116,6 +141,124 @@ app.delete('/api/produits/:id', (req, res) => {
       res.status(500).json({ error: 'Error deleting product' });
     });
 });
+
+
+
+
+//===================================User=====================================
+// Route pour ajouter un nouvel utilisateur
+app.post('/api/users', async (req, res) => {
+    const { nom, prenom, tel, email, motDePasse } = req.body;
+  
+    // Vérification des données obligatoires
+    if (!nom || !prenom || !tel || !email || !motDePasse) {
+      return res.status(400).json({ error: 'Veuillez fournir tous les champs obligatoires' });
+    }
+  
+    try {
+      // Vérifier si l'utilisateur existe déjà
+      const existingUser = await User.findOne({ email: email });
+      if (existingUser) {
+        return res.status(409).json({ error: 'Cet email est déjà utilisé par un autre utilisateur' });
+      }
+  
+      // Chiffrer le mot de passe avec bcrypt
+      const hashedPassword = await bcrypt.hash(motDePasse, 10);
+  
+      // Créer un nouvel utilisateur
+      const newUser = new User({
+        nom: nom,
+        prenom: prenom,
+        tel: tel,
+        email: email,
+        motDePasse: hashedPassword,
+        panier: []
+      });
+  
+      // Enregistrer le nouvel utilisateur dans la base de données
+      await newUser.save();
+  
+      res.status(201).json({ message: 'Utilisateur ajouté avec succès' });
+    } catch (error) {
+      console.error('Error adding user:', error);
+      res.status(500).json({ error: 'Error adding user' });
+    }
+  });
+  
+  // Route pour récupérer tous les utilisateurs
+  app.get('/api/users', async (req, res) => {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+      res.status(500).json({ error: 'Error fetching users' });
+    }
+  });
+  
+  // Route pour récupérer un utilisateur par son ID
+  app.get('/api/users/:id', async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const user = await User.findById(id);
+      if (!user) {
+        return res.status(404).json({ error: 'Utilisateur non trouvé' });
+      }
+      res.json(user);
+    } catch (error) {
+      console.error('Error fetching user:', error);
+      res.status(500).json({ error: 'Error fetching user' });
+    }
+  });
+  
+  // Route pour mettre à jour un utilisateur
+  app.put('/api/users/:id', async (req, res) => {
+    const { id } = req.params;
+    const { nom, prenom, tel, email, motDePasse } = req.body;
+  
+    try {
+      const user = await User.findById(id);
+      if (!user) {
+        return res.status(404).json({ error: 'Utilisateur non trouvé' });
+      }
+  
+      // Mettre à jour les champs modifiables de l'utilisateur
+      user.nom = nom || user.nom;
+      user.prenom = prenom || user.prenom;
+      user.tel = tel || user.tel;
+      user.email = email || user.email;
+  
+      // Vérifier si un nouveau mot de passe est fourni
+      if (motDePasse) {
+        const hashedPassword = await bcrypt.hash(motDePasse, 10);
+        user.motDePasse = hashedPassword;
+      }
+  
+      await user.save();
+      res.json(user);
+    } catch (error) {
+      console.error('Error updating user:', error);
+      res.status(500).json({ error: 'Error updating user' });
+    }
+  });
+  
+  // Route pour supprimer un utilisateur
+  app.delete('/api/users/:id', async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const user = await User.findByIdAndRemove(id);
+      if (!user) {
+        return res.status(404).json({ error: 'Utilisateur non trouvé' });
+      }
+      res.json({ message: 'Utilisateur supprimé avec succès' });
+    } catch (error) {
+      console.error('Error deleting user:', error);
+      res.status(500).json({ error: 'Error deleting user' });
+    }
+  });
+//============================================================================
 
 // Démarrage du serveur et écoute des requêtes entrantes
 app.listen(port, () => {
